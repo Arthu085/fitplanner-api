a0864310b9616774b204469e13eb4f977485cb1e
Arthur Ghizi <arthurghizi22@gmail.com>
2025-06-03 14:36:17 -0300

    models do banco adicionado

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const prisma = new PrismaClient();
 const PORT = 3000;
+const apiRoute = "/api";
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("API está funcionando");
-});
-
-app.get("/test-connection", async (req, res) => {
-	try {
-		res.json({ success: true, message: "Conexão com banco funcionando" });
-	} catch (error) {
-		console.error("Erro na conexão com o banco:", error);
-		res
-			.status(500)
-			.json({ success: false, message: "Erro na conexão com o banco" });
-	}
-});
+app.get(apiRoute, (req, res) => res.send("API funcionando"));
 
 app.listen(PORT, () => {
 	console.log(`Servidor rodando em http://localhost:${PORT}`);
